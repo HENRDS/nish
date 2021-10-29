bb3aefec1d2ebc1b40d32796eafe4505c234bd6e
Henry Rodrigues Da Silva <henryqx@hotmail.com>
2021-10-29 19:14:40 -0300

    Start writing lexer

--- src/shellparse.nim
+++ src/shellparse.nim
@@ -1,33 +1,50 @@
 import lexbase, strutils, ropes, sugar
 
 
+const 
+  InvalidChar = '\xff';
 type
-  NodeKind = enum
-    nkCmd, nkPipe, nkVar, nkQuoted
-  Node {.acyclic.} = ref object 
-    data: string
-    children: seq[Node]
-  CommandParser = object of BaseLexer
-    parent: Node
+  NodeKind* = enum
+    nkCmd, nkPipe, nkVar, nkArg
+  Node* {.acyclic.} = ref object 
+    kind*: NodeKind
+    data*: string
+    children*: seq[Node]
+  
+  ShellLexer* = object
+    bufPos: int
+    buf: string
+    lexemeStart: int
+    
 
 
+proc initShellLexer*(): ShellLexer =
+  ShellLexer(bufPos: 0, lexemeStart: 0, buf: "")
 
+proc reset*(l: var ShellLexer, newBuf: string)=
+  l.bufPos = 0
+  l.lexemeStart = 0
+  l.buf = newBuf
 
-proc shellSplit*(s: string): seq[string]= 
-  var
-    buf = ""
-    inStr = false
-  result = @[]
-  for c in s:
-    case c
-    of ' ':
-      if inStr:
-        buf &= c
-      else:
-        result.add(buf)
-        buf = ""
-    else:
-      buf &= c
-  if len(result) == 0:
-    result.add(s)
+template isAtEnd(l: ShellLexer): bool = 
+  l.bufPos >= l.buf.len()
 
+template current(l: ShellLexer): char =
+  if l.isAtEnd(): 
+    InvalidChar
+  else:
+    l.buf[l.bufPos]
+  
+proc advance(l: var ShellLexer)=
+  l.bufPos += 1
+
+proc match(l: var ShellLexer, options: varargs[char]): bool =
+  if options.contains(l.current):
+    l.advance()
+    return true
+  return false
+
+proc lexText(l: var ShellLexer): string =
+  discard
+proc nextToken(l: var ShellLexer): string =
+  discard
